82ea6c52fd6da8f9381299074b41a9af0e39d32f
Andrey Pudov <syscreat@gmail.com>
2012-10-19 23:58:10 +0400

    Start working on application modules.

---
+++ src/com/intellivision/ui/modules/HelpModule.java
@@ -0,0 +1,80 @@
+/*
+ * IntelliVision Intelligence Image Processing System
+ *
+ * The MIT License
+ *
+ * Copyright 2011-2012 Andrey Pudov.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.intellivision.ui.modules;
+
+import com.intellivision.util.StatusCodes;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.HBox;
+
+/**
+ * Brief description about application functionality, development team,
+ * and application license.
+ *
+ * @author    Andrey Pudov        <mail@andreypudov.com>
+ * @version   0.00.00
+ * %name      HelpModule.java
+ * %date      07:00:00 PM, Oct 19, 2012
+ */
+public class HelpModule extends HBox implements AbstractModule {
+
+    private static final java.util.logging.Logger LOG
+            = java.util.logging.Logger.getLogger("IntelliVision");
+
+    /* the instance of the help module */
+    private static volatile HelpModule INSTANCE;
+
+    private HelpModule() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                    "/com/intellivision/resources/schemas/HelpModule.fxml"));
+
+        fxmlLoader.setRoot(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (java.io.IOException e) {
+            LOG.severe(e.getMessage());
+            System.exit(StatusCodes.EXIT_FAILURE);
+        }
+    }
+
+    /**
+     * Returns an instance of help module.
+     *
+     * @return the instance of this module.
+     */
+    public static HelpModule getInstance() {
+        if (INSTANCE == null) {
+            synchronized (HelpModule.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new HelpModule();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+}
---
+++ src/com/intellivision/util/pools/Modules.java
@@ -0,0 +1,63 @@
+/*
+ * IntelliVision Intelligence Image Processing System
+ *
+ * The MIT License
+ *
+ * Copyright 2011-2012 Andrey Pudov.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.intellivision.util.pools;
+
+import com.intellivision.ui.modules.AbstractModule;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Modules pool provides functionality to navigate through application modules,
+ * such as methods to add and remove existing modules from this pool.
+ *
+ * @author    Andrey Pudov        <mail@andreypudov.com>
+ * @version   0.00.00
+ * %name      Modules.java
+ * %date      07:00:00 PM, Oct 19, 2012
+ */
+public class Modules {
+
+    private static final java.util.logging.Logger LOG
+            = java.util.logging.Logger.getLogger("IntelliVision");
+
+    /* a stack of application modules */
+    private static Deque<AbstractModule> modules = new ArrayDeque<>(4);
+
+    /* do not let anyone instantiate this class */
+    private Modules() {
+    }
+
+    /**
+     * Pushes an element onto the modules pool.
+     *
+     * @param module the application module.
+     */
+    public static void addModule(final AbstractModule module) {
+        modules.push(module);
+    }
+
+}

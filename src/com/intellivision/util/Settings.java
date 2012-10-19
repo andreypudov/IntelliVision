278976141f8a6d790a784aaff2d13d4d60241ee0
Andrey Pudov <syscreat@gmail.com>
2012-10-19 15:45:21 +0400

    Fixes in Settings code - previously first initialization creates a trace of errors at launch without ability to save settings on exit.

--- src/com/intellivision/util/Settings.java
+++ src/com/intellivision/util/Settings.java
@@ -1,5 +1,5 @@
 /*
- * IntelliVision Intelligence Image Processing System 
+ * IntelliVision Intelligence Image Processing System
  *
  * The MIT License
  *
@@ -33,37 +33,37 @@ import java.io.IOException;
 import java.util.Properties;
 
 /**
- * Provides a persistent set of properties. 
- * 
+ * Provides a persistent set of properties.
+ *
  * @author    Andrey Pudov        <mail@andreypudov.com>
  * @version   0.00.00
  * %name      Settings.java
  * %date      06:00:00 PM, Sep 12, 2012
  */
 public enum Settings {
-    
+
     INSTANCE;
-    
-    private static final java.util.logging.Logger LOG 
+
+    private static final java.util.logging.Logger LOG
             = java.util.logging.Logger.getLogger("IntelliVision");
-    
+
     private static final Properties properties = new Properties();
-    
+
     static {
-        /* 
-         * Initialize application settings with default values and load 
+        /*
+         * Initialize application settings with default values and load
          * applicatino settings from configuration file (if file is not exists,
          * default values will be used).
          */
         init();
-        
+
         File[] directoryList = {
             /* .IntelliVision */
             new File(System.getProperty("user.home")
                 + System.getProperty("file.separator")
                 + ".IntelliVision")
         };
-        
+
         File[] fileList = {
             /* .IntelliVision/.settings */
             new File(System.getProperty("user.home")
@@ -72,16 +72,16 @@ public enum Settings {
                 + System.getProperty("file.separator")
                 + ".settings")
         };
-        
+
         for (File directory : directoryList) {
             if (!(directory.exists()) || !(directory.isDirectory())) {
-                if (!(directory.delete()) || !(directory.mkdirs())) {
+                if (!(directory.delete()) && !(directory.mkdirs())) {
                     LOG.warning("Application settings initializaition failed!"
                             + " Default values will be used.");
                 }
             }
         }
-        
+
         for (File file : fileList) {
             if (!(file.exists()) || !(file.isFile())) {
                 try {
@@ -94,45 +94,45 @@ public enum Settings {
             }
         }
     }
-    
+
     /* do not let anyone instantiate this class */
     private Settings() {
     }
-    
+
     /**
      * Returns an instance of application settings list.
-     * 
+     *
      * @return the suitable settings list.
      */
     public static Settings getSettings() {
         return Settings.INSTANCE;
     }
-    
+
     /**
-     * Searches for the property with the specified key in this property list. 
+     * Searches for the property with the specified key in this property list.
      * The method returns null if the property is not found.
-     * 
+     *
      * @param key the key whose associated value is to be returned.
-     * @return    the value to which the specified key is mapped, or null if 
+     * @return    the value to which the specified key is mapped, or null if
      *            this map contains no mapping for the key.
      */
     public String getValue(String key) {
         return properties.getProperty(key);
     }
-       
+
     /**
-     * Sets property value for the specified key. The method returns the 
+     * Sets property value for the specified key. The method returns the
      * previous value of the specified key.
-     * 
+     *
      * @param key   the key to be placed into this property list.
-     * @param value the value corresponding to key. 
-     * @return      the previous value of the specified key in this property 
+     * @param value the value corresponding to key.
+     * @return      the previous value of the specified key in this property
      *              list, or null if it did not have one.
      */
     public String setValue(String key, String value) {
         return (String) properties.setProperty(key, value);
     }
-    
+
     /**
      * Initialize application settings.
      */
@@ -140,10 +140,10 @@ public enum Settings {
         File settings = new File(System.getProperty("user.home")
                 + System.getProperty("file.separator") + ".IntelliVision"
                 + System.getProperty("file.separator") + ".settings");
-        
+
         properties.setProperty("intellivision.window.width",  "1020.0");
         properties.setProperty("intellivision.window.height", "700.0");
-        
+
         try {
             properties.loadFromXML(new FileInputStream(settings));
         } catch (IOException e) {
@@ -152,7 +152,7 @@ public enum Settings {
                     ).append(e.getMessage()).toString());
         }
     }
-    
+
     /**
      * Reads application settings from configuration file.
      */
@@ -160,7 +160,7 @@ public enum Settings {
         File settings = new File(System.getProperty("user.home")
                 + System.getProperty("file.separator") + ".IntelliVision"
                 + System.getProperty("file.separator") + ".settings");
-        
+
         try {
             properties.loadFromXML(new FileInputStream(settings));
         } catch (IOException e) {
@@ -169,7 +169,7 @@ public enum Settings {
                     ).append(e.getMessage()).toString());
         }
     }
-    
+
     /**
      * Writes application settings to configuration files.
      */
@@ -177,7 +177,7 @@ public enum Settings {
         File settings = new File(System.getProperty("user.home")
                 + System.getProperty("file.separator") + ".IntelliVision"
                 + System.getProperty("file.separator") + ".settings");
-        
+
         try {
             properties.storeToXML(new FileOutputStream(settings),
                     "IntelliVision Intelligence Image Processing System");

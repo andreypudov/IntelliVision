6670a3e59a4bdb0647cdddd20756cd4c2fd70f28
Andrey Pudov <andrey@andreypudov.com>
2014-01-16 20:55:26 +0400

    Page jumping has been fixed to avoid changes in layout when scroll bar appears for a long pages.

--- subprojects/OnlineAthletics/src/java/com/onlineathletics/web/ContactForm.java
+++ subprojects/OnlineAthletics/src/java/com/onlineathletics/web/ContactForm.java
@@ -28,7 +28,6 @@ package com.onlineathletics.web;
 
 import com.onlineathletics.util.Validator;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;

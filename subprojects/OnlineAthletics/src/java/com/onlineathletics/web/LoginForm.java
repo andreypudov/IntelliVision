0a19c29eaf187436509077a63b372d63da94b839
Andrey Pudov <andrey@andreypudov.com>
2014-02-13 23:31:46 +0400

    The first working version of login form servlet is done.

--- subprojects/OnlineAthletics/src/java/com/onlineathletics/web/LoginForm.java
+++ subprojects/OnlineAthletics/src/java/com/onlineathletics/web/LoginForm.java
@@ -26,14 +26,23 @@
 
 package com.onlineathletics.web;
 
-import com.sun.enterprise.security.ee.auth.login.ProgrammaticLogin;
+import com.onlineathletics.util.Messages;
+import com.onlineathletics.util.Validator;
 import java.io.IOException;
-import java.util.logging.Level;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
 import javax.inject.Named;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * The authentication servlet provides ability to authenticate a user.
@@ -44,47 +53,108 @@ import javax.servlet.http.HttpServletResponse;
  * %date      09:40:00 PM, Feb 11, 2014
  */
 @Named("loginForm")
-public class LoginForm extends HttpServlet {
+@SessionScoped
+public class LoginForm implements Serializable {
     
-    private static final long serialVersionUID = 0x638c_498a_a92e_12adL;
+    private static final long serialVersionUID = 1L;
     private static final java.util.logging.Logger LOG
             = java.util.logging.Logger.getLogger(
             com.onlineathletics.core.Manifest.NAME);
     
-    @Override
-    protected void doGet(final HttpServletRequest request, 
-            final HttpServletResponse response) 
-            throws ServletException, IOException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    private static final String HOME_PAGE_URL = "faces/index.xhtml";
+    
+    private boolean usernameValid = true;
+    private boolean passwordValid = true;
+    
+    private String username;
+    private String password;
+    private String webpage;
+    
+    @PostConstruct
+    public void init() {
+        final FacesContext context = FacesContext.getCurrentInstance();
+        
+        webpage = (String) context.getExternalContext().getRequestMap()
+                .get(RequestDispatcher.FORWARD_REQUEST_URI);
     }
     
-    @Override
-    protected void doPost(final HttpServletRequest request, 
-            final HttpServletResponse response)
-            throws ServletException, IOException {
-        final ProgrammaticLogin login = new ProgrammaticLogin();
+    public void login() {
+        final FacesContext       context = FacesContext.getCurrentInstance();
+        final HttpServletRequest request = (HttpServletRequest) 
+                context.getExternalContext().getRequest();
         
         try {
-            final Boolean authneticated = login.login(
-                    request.getParameter("j_username"), 
-                    request.getParameter("j_password").toCharArray(), 
-                    "OnlineAthleticsRealm", request, response, true);
-            
-            if ((authneticated != null) && (authneticated.booleanValue())) {
-                response.sendRedirect("index.xhtml");
-                return;
-            }
-        } catch (final Exception e) {
-            LOG.log(Level.SEVERE, 
-                    "Authentication servlet failed with follwing error: {0}", 
-                    e.getMessage());
+            request.login(username, password);
+            context.getExternalContext().redirect(webpage);
+        } catch (final ServletException | IOException e) {
+            //context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Bad login! " + webpage, null));
         }
-        
-        response.sendRedirect("login.xhtml?auth=failed");
     }
     
-    @Override
-    public String getServletInfo() {
-        return "Login servlet.";
+    public String getUsername() {
+        return this.username;
+    }
+    
+    public String getPassword() {
+        return this.password;
+    }
+    
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+    
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+    
+    /**
+     * Returns the account name validation status.
+     * 
+     * @return the account name validation status.
+     */
+    public boolean isUsernameValid() {
+        return usernameValid;
+    }
+    
+    /**
+     * Returns the account password validation status.
+     * 
+     * @return the account password validation status.
+     */
+    public boolean isPasswordValid() {
+        return passwordValid;
+    }
+    
+    /**
+     * Validates the contact form elements and returns the form to the browser
+     * if any elements contains invalid values.
+     * 
+     * @param event the source of a system event.
+     */
+    public void validate(final ComponentSystemEvent event) {
+        final FacesContext context   = FacesContext.getCurrentInstance();
+        final UIComponent  component = event.getComponent();
+        
+        /* contact name field */
+        final UIInput username = (UIInput) component.findComponent("j_username");
+        final UIInput password = (UIInput) component.findComponent("j_password");
+        
+        final FacesMessage nameMissing   = Messages.getMessage("name_is_missing");
+        final FacesMessage nameOverlong  = Messages.getMessage("name_is_overlong");
+        
+        final FacesMessage emailMissing  = Messages.getMessage("email_is_missing");
+        final FacesMessage emailOverlong = Messages.getMessage("email_is_overlong");
+        
+        usernameValid  = Validator.validateText(username, 
+                Validator.CONTACT_NAME_MAX_LENGTH, nameMissing, nameOverlong, 
+                context);
+        passwordValid = Validator.validateText(password,
+                Validator.CONTACT_NAME_MAX_LENGTH, emailMissing, emailOverlong, 
+                context);
+        
+        /* return submit request with an error description */
+        if ((usernameValid == false) || (passwordValid == false)) {
+            context.renderResponse();
+        }
     }
 }
